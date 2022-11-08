3c32ba7664e98f466b3dfe982065e5518d9656ee
xxz <xxz911@rambler.ru>
2022-11-08 16:01:17 +0300

    Created Unit test(serializers, api)

---
+++ books/store/tests/test_serializers.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from store.serializers import BooksSerializer
+from store.models import Book
+
+
+
+class BooksSerializerTestCase(TestCase):
+    def test_ok(self):
+        book_1 = Book.objects.create(name='Test book 1', price=25)
+        book_2 = Book.objects.create(name='Test book 2', price=45)
+        data = BooksSerializer([book_1, book_2], many=True).data
+        expected_data = [
+            {
+                'id': book_1.id,
+                'name': 'Test book 1',
+                'price': '25.00'
+
+            },
+            {
+                'id': book_2.id,
+                'name': 'Test book 2',
+                'price': '45.00'
+
+            }
+        ]
+        self.assertEqual(expected_data, data)
